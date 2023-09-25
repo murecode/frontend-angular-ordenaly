5e2d6ded345e5a63ccefc8405079f2e445caf4df
muredev <jerico9222@hotmail.com>
2023-09-25 12:43:25 -0500

    cambios menores

--- src/app/layout/layout.routes.ts
+++ src/app/layout/layout.routes.ts
@@ -1,15 +1,9 @@
 import { Route } from "@angular/router";
 import { LayoutPageComponent } from "./layout-page.component";
-import { ProductPageComponent } from "../models/products/product-page.component";
-import { OrderPageComponent } from "../models/orders/order-page.component";
 
 export default [
   {
     'path': '',
     'component': LayoutPageComponent,
-    'children': [
-      { path: 'orders', component: OrderPageComponent },
-      { path: 'products', component: ProductPageComponent }
-    ]
   }
 ] as [Route]
