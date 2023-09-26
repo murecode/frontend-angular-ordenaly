a986fcc2d7dece28049bd63631b1ba5cdc029d4c
muredev <jerico9222@hotmail.com>
2023-09-26 18:37:45 -0500

    Se agregaron nuevos componentes

--- src/app/models/orders/list-page/list-page.component.ts
+++ src/app/models/orders/list-page/list-page.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { OrderCardComponent } from 'src/app/shared/components/order-card/order-card.component';
+import { FilterTabsComponent } from 'src/app/shared/components/fiterTabs/filter-tabs.component';
 
 @Component({
   selector: 'app-list-page',
   standalone: true,
-  imports: [CommonModule, OrderCardComponent],
+  imports: [CommonModule, OrderCardComponent, FilterTabsComponent],
   templateUrl: './list-page.component.html',
   styles: [
   ]
--- src/app/shared/components/sidebar/sidebar.component.ts
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -1,14 +1,13 @@
 import { Component } from '@angular/core';
-import { MatSidenavContainer, MatSidenavModule } from '@angular/material/sidenav';
-import { MatListItem, MatListModule } from '@angular/material/list';
-import { MatIcon } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
 
 
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [ MatSidenavModule, MatListModule,  ],
+  imports: [ MatListModule, MatIconModule ],
   templateUrl: './sidebar.component.html',
 })
 export class SidebarComponent {
