7b56cb8f87f39f668934bdf7aa7d3eae732724e8
Adalberto <9222amr@gmail.com>
2024-07-03 14:16:33 -0500

    update

---
+++ src/app/layout/layout.component.ts
@@ -0,0 +1,16 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'add-button',
+  standalone: true,
+  imports: [
+  
+  ],
+  templateUrl: 'layout.component.html',
+  styles: [
+  ]
+
+})
+export class LayoutComponent {
+
+}
